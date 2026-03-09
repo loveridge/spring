4fb678542d0035ee68464923c6b834f68330c495
Michael <git@lvrde.com>
2026-03-08 23:52:51 -0700

    performance check

--- test/engine/Sim/Misc/CollisionHandlerBoxVolume_test.cpp
+++ test/engine/Sim/Misc/CollisionHandlerBoxVolume_test.cpp
@@ -1,8 +1,12 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
+#include <array>
+
 #include "Sim/Misc/CollisionHandler.h"
 #include "Sim/Misc/CollisionVolume.h"
+#include "System/Log/ILog.h"
 #include "System/Matrix44f.h"
+#include "System/TimeProfiler.h"
 #include "System/float3.h"
 #include "System/Misc/SpringTime.h"
 
@@ -599,4 +603,76 @@ TEST_CASE("CollisionHandler_IntersectBoxVolume_MultiAxisObliqueBoxes")
 		const CMatrix44f matB_Misses = MakeTransform(float3(4.5f, 4.5f, 4.5f), crazyRotation);
 		CHECK_FALSE(Intersects(boxA, matA, boxB, matB_Misses));
 	}
+}
+
+TEST_CASE("CollisionHandler_IntersectBoxVolume_AxisAligned_Performance")
+{
+	SKIP("performance");
+	const CollisionVolume box = MakeBoxVolume(float3(2.0f, 2.0f, 2.0f));
+	const CollisionVolume cylinder = MakeCylinderVolume(float3(2.0f, 2.0f, 6.0f), CollisionVolume::COLVOL_AXIS_Z);
+	const CMatrix44f boxMat = MakeTransform();
+
+	std::array<CMatrix44f, 64> hitMats;
+	std::array<CMatrix44f, 64> missMats;
+
+	for (std::size_t i = 0; i < hitMats.size(); ++i) {
+		const float x = 0.75f + ((i % 8) * 0.05f);
+		hitMats[i] = MakeTransform(float3(x, 2.0f, 4.0f));
+		missMats[i] = MakeTransform(float3(x, 2.2f, 4.0f));
+	}
+
+	const std::int64_t iterations = 10000000;
+	volatile std::int64_t sink = 0;
+
+	LOG("CollisionHandler axis-aligned box-volume:");
+	{
+		ScopedOnceTimer timer(" axis-aligned box vs cylinder (intersecting)");
+		for (std::int64_t j = iterations; j > 0; --j) {
+			sink ^= static_cast<std::int64_t>(Intersects(box, boxMat, cylinder, hitMats[j % hitMats.size()])) * j;
+		}
+	}
+	{
+		ScopedOnceTimer timer(" axis-aligned box vs cylinder (separated)");
+		for (std::int64_t j = iterations; j > 0; --j) {
+			sink ^= static_cast<std::int64_t>(Intersects(box, boxMat, cylinder, missMats[j % missMats.size()])) * j;
+		}
+	}
+
+	CHECK((sink | 1) != 0);
+}
+
+TEST_CASE("CollisionHandler_IntersectBoxVolume_Rotated_Performance")
+{
+	SKIP("performance");
+	const CollisionVolume box = MakeBoxVolume(float3(2.0f, 2.0f, 2.0f));
+	const CollisionVolume cylinder = MakeCylinderVolume(float3(2.0f, 2.0f, 6.0f), CollisionVolume::COLVOL_AXIS_Z);
+	const CMatrix44f boxMat = MakeTransform(ZeroVector, float3(0.0f, QUARTER_PI, 0.0f));
+
+	std::array<CMatrix44f, 64> hitMats;
+	std::array<CMatrix44f, 64> missMats;
+
+	for (std::size_t i = 0; i < hitMats.size(); ++i) {
+		const float y = 0.75f + ((i % 8) * 0.05f);
+		hitMats[i] = MakeTransform(float3(2.75f, y, 0.0f), float3(HALF_PI, 0.0f, 0.0f));
+		missMats[i] = MakeTransform(float3(3.6f, y, 0.0f), float3(HALF_PI, 0.0f, 0.0f));
+	}
+
+	const std::int64_t iterations = 10000000;
+	volatile std::int64_t sink = 0;
+
+	LOG("CollisionHandler rotated box-volume:");
+	{
+		ScopedOnceTimer timer(" rotated box vs cylinder (intersecting)");
+		for (std::int64_t j = iterations; j > 0; --j) {
+			sink ^= static_cast<std::int64_t>(Intersects(box, boxMat, cylinder, hitMats[j % hitMats.size()])) * j;
+		}
+	}
+	{
+		ScopedOnceTimer timer(" rotated box vs cylinder (separated)");
+		for (std::int64_t j = iterations; j > 0; --j) {
+			sink ^= static_cast<std::int64_t>(Intersects(box, boxMat, cylinder, missMats[j % missMats.size()])) * j;
+		}
+	}
+
+	CHECK((sink | 1) != 0);
 }
