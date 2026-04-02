b70586b36a8fb2b165c03de765aa87bebc674663
Michael <git@lvrde.com>
2026-04-01 21:43:28 -0700

    tracy

--- rts/System/Misc/TracyDefs.h
+++ rts/System/Misc/TracyDefs.h
@@ -3,7 +3,7 @@
 #include <tracy/Tracy.hpp>
 
 #ifdef RECOIL_DETAILED_TRACY_ZONING
-	#define RECOIL_DETAILED_TRACY_ZONE ZoneScoped
+	#define RECOIL_DETAILED_TRACY_ZONE ZoneNamed(TracyConcat(__recoil_tracy_zone_, TracyLine), true)
 #else
 	#define RECOIL_DETAILED_TRACY_ZONE do {} while(0)
 #endif
